a50a1a771a9509dfc52fe5b2e2a4ca26492547db
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-08-02 23:00:55 +0530

    Update ContactsContainer and ProfileInfo styles for responsiveness

--- src/components/chat/ProfileInfo.jsx
+++ src/components/chat/ProfileInfo.jsx
@@ -29,7 +29,7 @@ const ProfileInfo = () => {
     }
     return (
         <>
-            <div className="absolutte bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#2a2b33] ">
+            <div className="absolutte bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#2a2b33]">
                 <div className="flex gap-3 items-center justify-center ">
                     <div 
                     onClick={setOpenProfileModal}
